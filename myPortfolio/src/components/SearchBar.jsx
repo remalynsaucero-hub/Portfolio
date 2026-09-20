dc661a614844c4cb2114ea92bed6d78266e211ef
remalynsaucero-hub <remalynsaucero@gmail.com>
2026-09-20 12:31:39 +0800

    some changes on searchbar

--- myPortfolio/src/components/SearchBar.jsx
+++ myPortfolio/src/components/SearchBar.jsx
@@ -1,16 +1,11 @@
-function Sidebar() {
+function SearchBar() {
   return (
-    <aside className="sidebar">
-      <h2>Menu</h2>
-
-      <ul>
-        <li>Dashboard</li>
-        <li>Students</li>
-        <li>Records</li>
-        <li>Settings</li>
-      </ul>
-    </aside>
+    <input
+      className="search"
+      type="text"
+      placeholder="Search student..."
+    />
   );
 }
 
-export default Sidebar;
+export default SearchBar;
