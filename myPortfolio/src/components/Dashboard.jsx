323156b9a81f27c788e079a4743d0d634306ca0c
remalynsaucero-hub <remalynsaucero@gmail.com>
2026-09-20 12:26:51 +0800

    adding input for dashboard

---
+++ myPortfolio/src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import Stats from "./Stats";
+import Welcome from "./Welcome";
+
+function Dashboard() {
+  return (
+    <section>
+      <Welcome />
+      <Stats />
+    </section>
+  );
+}
+
+export default Dashboard;
