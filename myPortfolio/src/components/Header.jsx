657459458a835dee5d85ecdfea6afbf9fbb29db0
remalynsaucero-hub <remalynsaucero@gmail.com>
2026-09-20 12:23:18 +0800

    adding data for header

---
+++ myPortfolio/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>Student Record System</h1>
+      <p>Manage Student Information</p>
+    </header>
+  );
+}
+
+export default Header;
