40b5745b8157ee424b75b85feef1e92989fce1c8
remalynsaucero-hub <remalynsaucero@gmail.com>
2026-09-20 12:29:35 +0800

    input data for Stats.jsx

---
+++ myPortfolio/src/components/Stats.jsx
@@ -0,0 +1,22 @@
+function Stats() {
+  return (
+    <div className="stats">
+      <div className="stat-card">
+        <h3>10</h3>
+        <p>Total Students</p>
+      </div>
+
+      <div className="stat-card">
+        <h3>8</h3>
+        <p>Active Students</p>
+      </div>
+
+      <div className="stat-card">
+        <h3>2</h3>
+        <p>Inactive Students</p>
+      </div>
+    </div>
+  );
+}
+
+export default Stats;
