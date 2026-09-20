48931468472a9da962218aaf09ac188a63a4bcea
remalynsaucero-hub <remalynsaucero@gmail.com>
2026-09-20 12:24:04 +0800

    adding button on sidebard

---
+++ myPortfolio/src/components/Welcome.jsx
@@ -0,0 +1,16 @@
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h2>Menu</h2>
+
+      <ul>
+        <li>Dashboard</li>
+        <li>Students</li>
+        <li>Records</li>
+        <li>Settings</li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
